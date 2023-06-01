96bdb2ca6756edbf017fb21b9516ec5bd8d4545b
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-06-01 16:18:09 +0600

    Feat:Sidebar added

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -11,7 +11,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames (cls.navbar, {}, [className])}>
-            <ThemeSwitcher />
+
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
                     Главная
