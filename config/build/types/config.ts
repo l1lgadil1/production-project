966d1d1f7858aaed6a9b6123436bbf19aec90eb7
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-06-01 11:18:02 +0600

    feature:Feature slices based design

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -3,8 +3,9 @@ export type BuildMode = 'production' | 'development';
 
 export interface BuildPaths {
     entry: string;
-     build: string;
+    build: string;
     html: string;
+    src: string;
 }
 
 export interface BuildEnv {
