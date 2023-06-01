4742a4b1e2dfd4c6bf6965dc9f6645ea8304c30e
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-06-01 17:01:38 +0600

    Feat:language switcher

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Sidebar.module.scss'
 import React, {useState} from "react";
 import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
+import {LangSwitcher} from "widgets/LangSwitcher";
 
 interface SidebarProps {
     className?: string;
@@ -16,7 +17,7 @@ export const Sidebar = ({className}: SidebarProps) => {
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher/>
-                {/*    Lang switcher*/}
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
