81cfa889a1ebf2d9209b640587ee4c6ebc63a5dd
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-06-04 09:47:17 +0600

    Feat:storybook added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,48 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        jest: true
-    },
-    extends: [
-        'standard-with-typescript',
-        'plugin:react/recommended'
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        project: './tsconfig.json'
-    },
-    plugins: [
-        'react',
-        '@typescript-eslint',
-        'i18next'
-
-    ],
-    rules: {
-        'react/jsx-indent': [2, 4],
-        'react/jsx-indent-props': [2, 4],
-        // indent: ['error', 4, { SwitchCase: 1, ignoredNodes: ['PropertyDefinition'] }],
-        indent: 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/strict-boolean-expressions': 'off',
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-        'import-no-unresolved': 'off',
-        'import/prefer-default-export': 'off',
-        'no-unused-vars': 'off',
-        'react/require-default-props': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react/jsx-props-no-spreading': 'warn',
-        '@typescript-eslint/naming-convention': 'off',
-        'react/function-component-definition': 'off',
-        'no-shadow': 'off',
-        '@typescript-eslint/no-floating-promises': 'off',
-        'no-underscore-dangle': 'off',
-        ' @typescript-eslint/naming-convention': 'off',
-        '@typescript-eslint/prefer-nullish-coalescing': 'off',
-        '@typescript-eslint/no-misused-promises': 'off',
-        "max-len": ['error',{ignoreComments:true,code:100}]
-    },
-    globals: {
-        __IS_DEV__: true
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended', 'plugin:storybook/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: './tsconfig.json'
+  },
+  plugins: ['react', '@typescript-eslint', 'i18next'],
+  rules: {
+    'react/jsx-indent': [2, 4],
+    'react/jsx-indent-props': [2, 4],
+    // indent: ['error', 4, { SwitchCase: 1, ignoredNodes: ['PropertyDefinition'] }],
+    indent: 'off',
+    'i18next/no-literal-string': ['error', {
+      markupOnly: true
+    }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    'react/jsx-filename-extension': [2, {
+      extensions: ['.js', '.jsx', '.ts', '.tsx']
+    }],
+    'import-no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'off',
+    'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    '@typescript-eslint/naming-convention': 'off',
+    'react/function-component-definition': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    'no-underscore-dangle': 'off',
+    ' @typescript-eslint/naming-convention': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    "max-len": ['error', {
+      ignoreComments: true,
+      code: 100
+    }]
+  },
+  globals: {
+    __IS_DEV__: true
+  }
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,15 +1,15 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
-import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
-import { Button } from 'shared/ui/Button/Button';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useState} from 'react';
+import {ThemeSwitcher} from 'shared/ui/ThemeSwitcher';
+import {LangSwitcher} from 'shared/ui/LangSwitcher/LangSwitcher';
+import {Button} from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
     className?: string;
 }
 
-export const Sidebar = ({ className }: SidebarProps) => {
+export const Sidebar = ({className}: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
 
     const onToggle = () => {
@@ -19,7 +19,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
     return (
         <div
             data-testid="sidebar"
-            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
+            className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
         >
             <Button
                 data-testid="sidebar-toggle"
@@ -28,8 +28,8 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 toggle
             </Button>
             <div className={cls.switchers}>
-                <ThemeSwitcher />
-                <LangSwitcher className={cls.lang} />
+                <ThemeSwitcher/>
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
