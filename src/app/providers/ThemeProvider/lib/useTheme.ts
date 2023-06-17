bb03a27c07af4ae712e000c157873db01a5d986a
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-06-17 10:51:12 +0600

    Feat:orange theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+import {useContext} from 'react';
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from './ThemeContext';
 
 interface UseThemeResult {
     toggleTheme: () => void;
@@ -7,10 +7,24 @@ interface UseThemeResult {
 }
 
 export function useTheme(): UseThemeResult {
-    const { theme, setTheme } = useContext(ThemeContext);
+    const {theme, setTheme} = useContext(ThemeContext);
 
     const toggleTheme = () => {
-        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        let newTheme: Theme;
+        switch (theme) {
+            case Theme.DARK:
+                newTheme = Theme.LIGHT;
+                break;
+            case Theme.LIGHT:
+                newTheme = Theme.ORAGNE;
+                break;
+            case Theme.ORAGNE:
+                newTheme = Theme.DARK;
+                break;
+            default:
+                newTheme = Theme.LIGHT;
+                break;
+        }
         setTheme?.(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
