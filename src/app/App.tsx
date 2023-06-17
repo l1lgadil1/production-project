7a915d2eea54decefa87ad2c4aaea3914cc72f62
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-06-17 12:23:26 +0600

    Feat:auth check

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,17 @@
-import React, { Suspense, useEffect } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
-import { AppRouter } from 'app/providers/router';
-import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import React, {Suspense, useEffect} from 'react';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {useTheme} from 'app/providers/ThemeProvider';
+import {AppRouter} from 'app/providers/router';
+import {Navbar} from 'widgets/Navbar';
+import {Sidebar} from 'widgets/Sidebar';
+import {useDispatch, useSelector} from 'react-redux';
+import {getUserInited, userActions} from 'entities/User';
 
 function App() {
-    const { theme } = useTheme();
+    const {theme} = useTheme();
     const dispatch = useDispatch();
+    const inited = useSelector(getUserInited)
+
 
     useEffect(() => {
         dispatch(userActions.initAuthData());
@@ -18,10 +20,10 @@ function App() {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
-                <Navbar />
+                <Navbar/>
                 <div className="content-page">
-                    <Sidebar />
-                    <AppRouter />
+                    <Sidebar/>
+                    {inited && <AppRouter/>}
                 </div>
             </Suspense>
         </div>
