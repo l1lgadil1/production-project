75dec707b7194823d95b650b006d5fe696ca4d56
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-06-23 09:39:42 +0300

    Feat:articles page

--- src/pages/ArticleDetailPage/ui/ArticleDetailPage/ArticleDetailPage.async.tsx
+++
@@ -1,6 +0,0 @@
-import {lazy} from "react";
-
-export const ArticleDetailPageAsync = lazy(() => new Promise((resolve) =>
-    // @ts-ignore
-    setTimeout(() => resolve(import('./ArticleDetailPage')), 1500)
-))
