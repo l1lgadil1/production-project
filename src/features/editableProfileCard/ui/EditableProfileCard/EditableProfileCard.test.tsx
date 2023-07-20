f77714d93094c80760612d0636d6fe4e0f99874a
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-07-20 12:52:55 +0600

    feat:readme, jest fix

--- src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.test.tsx
@@ -1,12 +1,12 @@
-import {fireEvent, screen} from '@testing-library/react';
-import {componentRender} from '@/shared/lib/tests/componentRender/componentRender';
-import {EditableProfileCard} from "@/features/editableProfileCard";
-import {Profile} from "@/entities/Profile";
-import {Currency} from "@/entities/Currency";
-import {Country} from "@/entities/Country";
-import {profileReducer} from "@/features/editableProfileCard/model/slice/profileSlice";
+import {screen} from '@testing-library/react'
+import {componentRender} from '@/shared/lib/tests/componentRender/componentRender'
+import {EditableProfileCard} from '@/features/editableProfileCard'
+import {type Profile} from '@/entities/Profile'
+import {Currency} from '@/entities/Currency'
+import {Country} from '@/entities/Country'
+import {profileReducer} from '@/features/editableProfileCard/model/slice/profileSlice'
 import userEvent from '@testing-library/user-event'
-import {$api} from "@/shared/api/api";
+import {$api} from '@/shared/api/api'
 
 const profile: Profile = {
     id: '1',
@@ -17,7 +17,7 @@ const profile: Profile = {
     country: Country.Kazakhstan,
     city: 'Moscow',
     username: 'admin213'
-};
+}
 const options = {
     initialState: {
         profile: {
@@ -34,10 +34,10 @@ const options = {
     }
 }
 test('Readonly should switch', async () => {
-    componentRender(<EditableProfileCard id={'1'}/>, options);
+    componentRender(<EditableProfileCard id={'1'}/>, options)
     await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'))
     expect(screen.getByTestId('EditableProfileCardHeader.CancelButton')).toBeInTheDocument()
-});
+})
 test('Whilst cancel values should be resetted', async () => {
     componentRender(<EditableProfileCard id={'1'}/>, options)
     await userEvent.click(screen.getByTestId('EditableProfileCardHeader.EditButton'))
@@ -50,7 +50,7 @@ test('Whilst cancel values should be resetted', async () => {
     expect(screen.getByTestId('ProfileCard.firstname')).toHaveValue('user')
     expect(screen.getByTestId('ProfileCard.lastname')).toHaveValue('user')
 
-    await userEvent.click(screen.getByTestId('EditableProfileCardHeader.CancelButton'));
+    await userEvent.click(screen.getByTestId('EditableProfileCardHeader.CancelButton'))
 
     expect(screen.getByTestId('ProfileCard.firstname')).toHaveValue('admin')
     expect(screen.getByTestId('ProfileCard.lastname')).toHaveValue('admin')
@@ -66,7 +66,6 @@ test('Should return error', async () => {
     expect(screen.getByTestId('EditableProfileCard.Error.Paragraph')).toBeInTheDocument()
 })
 
-
 test('Should return PUT request', async () => {
     const mockPutReq = jest.spyOn($api, 'put')
 
@@ -77,5 +76,5 @@ test('Should return PUT request', async () => {
     await userEvent.type(screen.getByTestId('ProfileCard.firstname'), 'user')
     await userEvent.click(screen.getByTestId('EditableProfileCardHeader.SaveButton'))
 
-    expect(mockPutReq).toHaveBeenCalled();
+    expect(mockPutReq).toHaveBeenCalled()
 })
