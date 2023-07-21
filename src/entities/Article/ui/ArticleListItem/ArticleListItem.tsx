d485a09996666a74e26f05433c199cc9954b2424
Ilyas Agadilov <agadilov.ilyas@mail.ru>
2023-07-21 13:10:13 +0600

    feat:image lazy component

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,21 +1,23 @@
-import  { classNames } from '@/shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { HTMLAttributeAnchorTarget, memo, useCallback } from 'react';
-import { Text } from '@/shared/ui/Text/Text';
-import { Icon } from '@/shared/ui/Icon/Icon';
+import {classNames} from '@/shared/lib/classNames/classNames';
+import {useTranslation} from 'react-i18next';
+import {HTMLAttributeAnchorTarget, memo, useCallback} from 'react';
+import {Text} from '@/shared/ui/Text/Text';
+import {Icon} from '@/shared/ui/Icon/Icon';
 import EyeIcon from '@/shared/assets/icons/eye-20-20.svg';
-import { Card } from '@/shared/ui/Card/Card';
-import { Avatar } from '@/shared/ui/Avatar/Avatar';
-import { Button, ButtonTheme } from '@/shared/ui/Button/Button';
-import { useNavigate } from 'react-router-dom';
+import {Card} from '@/shared/ui/Card/Card';
+import {Avatar} from '@/shared/ui/Avatar/Avatar';
+import {Button, ButtonTheme} from '@/shared/ui/Button/Button';
+import {useNavigate} from 'react-router-dom';
 import {getRouteArticleDetails, RoutePath} from '@/shared/config/routeConfig/routeConfig';
-import { AppLink } from '@/shared/ui/AppLink/AppLink';
+import {AppLink} from '@/shared/ui/AppLink/AppLink';
 import cls from './ArticleListItem.module.scss';
 import {
     Article, ArticleTextBlock,
 } from '../../model/types/article';
-import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
+import {ArticleTextBlockComponent} from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
 import {ArticleBlockType, ArticleView} from "@/entities/Article/model/const/consts";
+import {AppImage} from "@/shared/ui/AppImage";
+import {Skeleton} from "@/shared/ui/Skeleton/Skeleton";
 
 interface ArticleListItemProps {
     className?: string;
@@ -28,13 +30,13 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
     const {
         className, article, view, target,
     } = props;
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
-    const types = <Text text={article.type.join(', ')} className={cls.types} />;
+    const types = <Text text={article.type.join(', ')} className={cls.types}/>;
     const views = (
         <>
-            <Text text={String(article.views)} className={cls.views} />
-            <Icon Svg={EyeIcon} />
+            <Text text={String(article.views)} className={cls.views}/>
+            <Icon Svg={EyeIcon}/>
         </>
     );
 
@@ -47,15 +49,16 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
             <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
                 <Card className={cls.card}>
                     <div className={cls.header}>
-                        <Avatar size={30} src={article.user.avatar} />
-                        <Text text={article.user.username} className={cls.username} />
-                        <Text text={article.createdAt} className={cls.date} />
+                        <Avatar size={30} src={article.user.avatar}/>
+                        <Text text={article.user.username} className={cls.username}/>
+                        <Text text={article.createdAt} className={cls.date}/>
                     </div>
-                    <Text title={article.title} className={cls.title} />
+                    <Text title={article.title} className={cls.title}/>
                     {types}
-                    <img src={article.img} className={cls.img} alt={article.title} />
+                    <AppImage fallback={<Skeleton width='100%' height={250}/>} src={article.img} className={cls.img}
+                              alt={article.title}/>
                     {textBlock && (
-                        <ArticleTextBlockComponent block={textBlock} className={cls.textBlock} />
+                        <ArticleTextBlockComponent block={textBlock} className={cls.textBlock}/>
                     )}
                     <div className={cls.footer}>
                         <AppLink
@@ -81,14 +84,15 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
         >
             <Card className={cls.card}>
                 <div className={cls.imageWrapper}>
-                    <img alt={article.title} src={article.img} className={cls.img} />
-                    <Text text={article.createdAt} className={cls.date} />
+                    <AppImage fallback={<Skeleton width='100%' height={250}/>} alt={article.title} src={article.img}
+                              className={cls.img}/>
+                    <Text text={article.createdAt} className={cls.date}/>
                 </div>
                 <div className={cls.infoWrapper}>
                     {types}
                     {views}
                 </div>
-                <Text text={article.title} className={cls.title} />
+                <Text text={article.title} className={cls.title}/>
             </Card>
         </AppLink>
     );
